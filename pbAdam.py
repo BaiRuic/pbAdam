08b2f83f98a53c69288381041fa15c59b054b9ef
Ruichang Bai <brc730@outlook.com>
2021-02-04 13:38:14 +0800

    first commit

---
+++ pbAdam.py
@@ -0,0 +1,102 @@
+import torch
+import torch.optim.functional  as F
+from torch.optim.optimizer import Optimizer
+
+
+class pbAdam(Optimizer):
+
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, gamma=0.9,
+                 weight_decay=0, amsgrad=False):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        if not 0.0 <= weight_decay:
+            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+        defaults = dict(lr=lr, betas=betas, eps=eps, gamma=gamma,
+                        weight_decay=weight_decay, amsgrad=amsgrad)
+        super(pbAdam, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(pbAdam, self).__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('amsgrad', False)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            params_with_grad = []
+            grads = []
+            exp_avgs = []
+            exp_avg_sqs = []
+            state_sums = []
+            max_exp_avg_sqs = []
+            state_steps = []
+
+            gamma = group['gamma']
+
+            for p in group['params']:
+                if p.grad is not None:
+                    params_with_grad.append(p)
+                    if p.grad.is_sparse:
+                        raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
+                    
+                    d_p = (torch.sign(p.grad) * torch.pow(torch.abs(p.grad), gamma)).data
+                    grads.append(d_p)
+
+                    state = self.state[p]
+                    # Lazy state initialization
+                    if len(state) == 0:
+                        state['step'] = 0
+                        # Exponential moving average of gradient values
+                        state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                        # Exponential moving average of squared gradient values
+                        state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                        if group['amsgrad']:
+                            # Maintains max of all exp. moving avg. of sq. grad. values
+                            state['max_exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                    exp_avgs.append(state['exp_avg'])
+                    exp_avg_sqs.append(state['exp_avg_sq'])
+
+                    if group['amsgrad']:
+                        max_exp_avg_sqs.append(state['max_exp_avg_sq'])
+
+                    # update the steps for each param group update
+                    state['step'] += 1
+                    # record the step after step update
+                    state_steps.append(state['step'])
+
+            beta1, beta2 = group['betas']
+            F.adam(params_with_grad,
+                   grads,
+                   exp_avgs,
+                   exp_avg_sqs,
+                   max_exp_avg_sqs,
+                   state_steps,
+                   group['amsgrad'],
+                   beta1,
+                   beta2,
+                   group['lr'],
+                   group['weight_decay'],
+                   group['eps']
+                   )
+        return loss
+
+
+
---
+++ train.py
@@ -0,0 +1,107 @@
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import cnn
+import pbSGD
+import pbAdam
+
+
+def dataload(batch_size):
+    
+    # 数据预处理。transforms.ToTensor()将图片转换成PyTorch中处理的对象Tensor,并且进行标准化（数据在0~1之间）
+    # transforms.Normalize()做归一化。它进行了减均值，再除以标准差。两个参数分别是均值和标准差
+    # transforms.Compose()函数则是将各种预处理的操作组合到了一起
+    
+    data_tf = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])])
+
+    # 数据集的下载器 如果下载过了， 参数download就是False
+    train_dataset = datasets.MNIST(
+        root='./data', train=True, transform=data_tf, download=False)
+    test_dataset = datasets.MNIST(root='./data', train=False, transform=data_tf)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
+def train(epoch):
+    
+    model.train()
+    train_ls = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_ls += loss.item()
+        _, predicted = outputs.max(1)
+        # 该批次 样本总数
+        total += targets.size(0)
+        # 计算正确率
+        correct += predicted.eq(targets).sum().item()
+    train_loss.append(train_ls/(batch_idx+1))
+    print('Train Loss: %.3f | Acc: %.3f%% (%d/%d)' % (train_ls/(batch_idx+1), 
+            100.*correct/total, correct, total))
+
+def test(epoch):
+    global best_acc
+    model.eval()
+    test_ls = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            test_ls += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+    test_loss.append(test_ls/(batch_idx+1))
+    print('Test Loss: %.3f | Acc: %.3f%% (%d/%d)' % (test_ls/(batch_idx+1), 100.*correct/total, correct, total))
+
+# 定义参数
+DEVICE = torch.device('gpu' if torch.cuda.is_available() else 'cpu')
+epochs = 3
+batch_size=64
+learning_rate = 0.02
+
+# 定义模型，损失函数，优化器
+model = cnn.CNN().to(DEVICE)
+criterion = nn.CrossEntropyLoss()
+# optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+# optimizer = pbSGD.pbSGD(model.parameters(), lr=learning_rate)
+optimizer = pbAdam.pbAdam(model.parameters(), lr=learning_rate)
+
+# 数据生成器
+trainloader,testloader = dataload(batch_size)
+
+# 训练模型
+train_loss = []
+test_loss = []
+for epoch in range(epochs):
+    print(f'\nEpoch: {epoch + 1} / {epochs}')
+    train(epoch)
+    test(epoch)
+
+# 绘图
+plt.figure()
+plt.plot(train_loss,label='train_loss')
+plt.plot(test_loss, label='valid_loss')
+plt.legend()
+plt.show()
+    
+
+
+
